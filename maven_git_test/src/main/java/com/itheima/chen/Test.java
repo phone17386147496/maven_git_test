88199c8e7adbe0466aa2b993370f35544df8787d
chenwei <251201429@qq.com>
2019-06-14 19:33:02 +0800

    change

---
+++ maven_git_test/src/main/java/com/itheima/chen/Test.java
@@ -0,0 +1,9 @@
+package com.itheima.chen;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(111);
+        System.out.println(222);
+        System.out.println(3333);
+    }
+}
